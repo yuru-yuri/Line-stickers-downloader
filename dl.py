a15efd239a0f7f526832498c55c6409dd3c306d2
1271 <sttv-pc@mail.ru>
2020-02-25 20:35:41 +0300

    Update

--- dl.py
+++ dl.py
@@ -1,19 +1,17 @@
 from requests import get
 from argparse import ArgumentParser, Namespace
 from lxml.html import document_fromstring, Element, HtmlElement
-from tinycss import make_parser
+from typing import List, Tuple
 import argcomplete
 import re
 from pathlib import Path
 from progressbar.bar import ProgressBar
+import json
 
 arguments = ArgumentParser()
-arguments.add_argument('idx', type=int, help='Get manga info from ids', nargs="*")
-arguments.add_argument('--url', type=str, help='Get manga info from ids', default='')
+arguments.add_argument('idx', type=int, help='Get info for ids', nargs="*")
+arguments.add_argument('--url', type=str, help='Download stickers for url', default='')
 arguments.add_argument('-d', '--destination', type=str, help='Destination folder', default='')
-arguments.add_argument(
-    '-a', '--animate', help='Download animate version', action='store_const', const=True, default=False
-)
 
 argcomplete.autocomplete(arguments)
 
@@ -37,60 +35,47 @@ class Downloader:
         for idx in self.indexes:  # type: int idx
             content = get(self.base_url.format(idx)).text
             parser = document_fromstring(content)  # type: HtmlElement parser
-            name = parser.cssselect('p + h3')[0].text
+            name = parser.cssselect('[class$="Top"] [class$="Head"] > *')[0].text
             print('Downloading {}'.format(name))
-            images = self.get_images(parser)
-            self.download(images, name)
+            static_images, animated_images = self.get_images(parser)
+            len(static_images) > 0 and self.download(static_images, name)
+            len(animated_images) > 0 and self.download(animated_images, name, True)
 
-    def get_images(self, parser):
-        elements = parser.cssselect('section[id] ul > li span[style]')
-        images = []
+    def get_images(self, parser) -> Tuple[List[str], List[str]]:
+        elements = parser.cssselect('[class$="Sticker"] [data-preview]')
+        static_images = []
+        animated_images = []
         for element in elements:  # type: Element element
-            value = self.get_image_url_from_style(element.get('style'))
-            if value is not None:
-                images.append(value)
-        return images
+            try:
+                data = json.loads(element.get('data-preview'))
+                if 'animation' == data['type']:
+                    animated_images.append(data['animationUrl'])
+                static_images.append(data['staticUrl'])
+            except Exception:
+                continue
+        return static_images, animated_images
 
     @staticmethod
     def url2idx(url: str) -> str:
         return re.search(r'/product/(\d+)/', url).group(1)
 
-    @staticmethod
-    def get_image_url_from_style(style: str):
-        css = make_parser()
-        value = None
-        for declaration in css.parse_style_attr(style)[0]:
-            if declaration.name == 'background':
-                for token in declaration.value:
-                    if token.type == 'URI':
-                        value = token.value
-                        break
-            if declaration.name == 'background-image':
-                value = declaration.value[0].value
-                break
-        return value
-
-    def download(self, images, name):
+    def download(self, images, name, animated = False):
         clear_name = re.sub(r'[^a-zA-Z\s]+', '-', name)
+        if animated:
+            clear_name += '_animated'
         dst = self.destination.joinpath(clear_name)
         if dst.is_dir():
             print('Destination exists. Skip')
             return
-        dst.mkdir(parents=True)
+        dst.mkdir(parents=True, exist_ok=True)
         progress = ProgressBar(max_value=len(images))
-        for idx, image in enumerate(images):
-            url = self.sticker_url(image)
+        for idx, url in enumerate(images):
             with open(str(dst.joinpath('{}.png'.format(idx + 1))), 'wb') as file:
                 file.write(get(url).content)
             progress.update(idx + 1)
-
-    def sticker_url(self, url):
-        animate_suffix = '/IOS/sticker_popup.png;compress=true'
-        if not self.args.animate:
-            return url
-        return re.sub(r'(.+?/\d+)/\w+/sticker.+', r'\1', url) + animate_suffix
 
 
 if __name__ == '__main__':
     downloader = Downloader(arguments)
     downloader.run()
+
